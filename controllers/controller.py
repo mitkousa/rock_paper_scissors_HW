0c279ad0f3117bba3d31c6076ef6394c60b551a8
mitkousa <uk.dimitar@gmail.com>
2021-08-08 19:22:15 +0100

    extension done

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,14 +3,25 @@ from app import app
 from models.game import *
 from models.player import *
 
-@app.route('/game')
+@app.route("/")
 def index():
     return render_template("index.html", games=games)
 
-# @app.route('/game/<choice>', methods=['POST'])
-# def add_player():
-#     player_name = request.form['name']
-#     player_choice = request.form['choice']
-#     new_task = Player(choice)
-#     player_choice(new_task)
-#     return render_template('index.html', name='name',choice='choice', games=games)
+@app.route("/welcome")
+def welcome_page():
+    return render_template("welcome.html", games=games)
+
+@app.route("/", methods=["POST"])
+def add_choice():
+    player_name = request.form["name"]
+    player_choice = request.form["choice"]
+    player_choice=Player(player_name, player_choice)
+    return render_template('index.html', name="name",choice="choice", games=games)
+
+@app.route("/Rock")
+def first_choice():
+    return render_template('first_selection.html', games=games)
+
+@app.route("/Rock/Scissors")
+def second_choice():
+    return render_template("result.html", games=games)
